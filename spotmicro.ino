6999040da323666b533934cb5bf3b8bde64d2d34
AugustDG <33916325+AugustDG@users.noreply.github.com>
2021-05-03 12:24:37 -0400

    Added Ultrasonic support, it now walks!
    Added ultrasonic sensor support (just reading data, no behavior based on that for now);
    Changed the walk routine for a better gait: it now walks! :D
    Added a macro for step size and changed the loop delay (to 0, for now) for smoother and faster movement!
    It now sets the shoulder servos at 0 on setup, so that it can actually work;
    Lowered general robot for better stability

--- spotmicro.ino
+++ spotmicro.ino
@@ -1,35 +1,44 @@
 #include <Adafruit_PWMServoDriver.h>
-
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#include <HCSR04.h>
 
 #define SERVO_FREQ 50       // Analog servos run at ~50 Hz updates
-#define PROCESSING_DELAY 25 // how much delay between loop calls (in ms)
+#define PROCESSING_DELAY 0 // how much delay between loop calls (in ms)
 
 // lengths of each leg segment
 #define UPPER_SEG_LENGTH 10
 #define LOWER_SEG_LENGTH 12
+#define STEP_SIZE 25 // how small is each degree step (larger it is, the smoother, but slower, it is)
 
 //basic struct to hold 2d coordinates
 struct Vector2D {
   float x, y;
 };
 
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+UltraSonicDistanceSensor sonicR(10, 11);
+UltraSonicDistanceSensor sonicL(12, 13);
+
 bool finishedMoving = false; //the bot has finished moving
 bool hasRoutine = false; //is following a routine
 int routineStepCount = 4; //how long the routines are
-int routineStep = 0; //where, in the routine, are we at?
+int routineStep = 0; //where, in the current routine, are we at?
+int routineLeg = 0; //what leg, in the current routine, is mainly doing the action?
 
 //bounds of each servo's pwm range
 // end, mid, shoulder;
-const int maxPwmVals[] = {230, 340, 310, 320, 150, 310,
-                          320, 150, 290, 230, 340, 310};
+const int maxPwmVals[] = {230, 340, 335, 
+                          320, 150, 295,
+                          330, 150, 315, 
+                          230, 340, 285};
 
-const int minPwmVals[] = {480, 140, 310, 70,  350, 310,
-                          70,  350, 290, 480, 140, 310};
+const int minPwmVals[] = {480, 140, 295, 
+                          70,  350, 335,
+                          80,  350, 275, 
+                          480, 140, 325};
 
 //upper bounds of each servo's degree range
-const int servoDegRanges[] = {115, 90, 0, 115, 90, 0, 115, 90, 0, 115, 90, 0};
+const int servoDegRanges[] = {115, 90, 10, 115, 90, 10, 115, 90, 10, 115, 90, 10};
 
 //steps each servo will take at each loop iteration
 float servoDegSteps[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -38,17 +47,17 @@ float desiredDegs[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //holds desired degr
 float currentDegs[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //holds the current degree values that each servo is set to
 
 //holds the positions of each leg (4) in 2 different positions, mostly for testing purposes
-struct Vector2D standingPos[] = {{0, 15}, {0, 15}, {0, 15}, {0, 15}};
+struct Vector2D standingPos[] = {{0, 12}, {0, 12}, {0, 12}, {0, 12}};
 struct Vector2D lyingPos[] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
 
 struct Vector2D* currentPos = new Vector2D[4]; //for easier testing, will remove later as dynamic memory on Arduino is limited
 struct Vector2D tempPos[] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}}; //holds positions for each leg until everyone has been assigned
 
-//holds each leg's position for the walk routine. difference with the positions array is that it ressembles a state machine for each leg
-struct Vector2D walkRoutine[][4] = {{{0, 15}, {0, 10}, {5, 10}, {5, 15}},
-                                    {{0, 15}, {0, 15}, {0, 15}, {0, 15}},
-                                    {{0, 15}, {0, 10}, {0, 10}, {0, 15}},
-                                    {{0, 15}, {0, 10}, {0, 10}, {0, 15}}};
+//holds each leg's position for the walk routine. difference with the positions array is that it ressembles a state machine for each leg: x(horizontal) is time and y(vertical) are the position states
+struct Vector2D walkRoutine[][4] = {{{0, 12}, {0, 10}, {7, 10}, {7, 12}},
+                                    {{0, 12}, {0, 12}, {0, 12}, {0, 12}},
+                                    {{0, 12}, {0, 10}, {7, 10}, {7, 12}},
+                                    {{0, 12}, {0, 12}, {0, 12}, {0, 12}}};
 
 void setup() {
   //open a serial connection and print starting string
@@ -61,6 +70,12 @@ void setup() {
   pwm.setPWMFreq(SERVO_FREQ); // Analog servos run at ~50 Hz updates
 
   delay(10);
+
+  //for simpler testing purposes, the shoulder servos are fixed for now
+  pwm.setPWM(2, 0, DegToPwm(2, 5));
+  pwm.setPWM(5, 0, DegToPwm(5, 5));
+  pwm.setPWM(8, 0, DegToPwm(8, 5));
+  pwm.setPWM(11, 0, DegToPwm(11, 5));
 
   //sets the starting position, need to make more foolproof for the final product
   currentPos = standingPos;
@@ -78,7 +93,7 @@ void CalculateRoutine()
 
   for (int i =0; i < 4; i++)
   {
-    tempPos[i] = walkRoutine[i][routineStep];
+    tempPos[i] = walkRoutine[RangeLoop(i+routineLeg, 4)][routineStep];
 
     continue; //remove this, testing purposes
 
@@ -86,13 +101,15 @@ void CalculateRoutine()
     Serial.print(i, DEC);
     Serial.print(", Routine #: ");
     Serial.print(routineStep, DEC);
+    Serial.print(", Routine Leg #: ");
+    Serial.print(routineLeg, DEC);
     Serial.print("; ");
     Serial.print(tempPos[i].x, DEC);
     Serial.print("/");
     Serial.println(tempPos[i].y, DEC);
     Serial.println("-----------------------");
 
-    delay(500);
+    delay(100);
   }
 
   currentPos = tempPos;
@@ -103,7 +120,15 @@ void CalculateRoutine()
   if (routineStep < routineStepCount - 1)
     routineStep++;
   else 
+  {
     routineStep = 0;
+
+    //after the steps in the routine is completed, chnages leg offset
+    if (routineLeg < 1)
+      routineLeg++;
+    else
+      routineLeg = 0;
+  }
 }
 
 //transforms the 2d coordinates to 2 angles (in degrees) for each servo and calculates the steps in degrees: needs to be called everytime we want the servos to move (when we change positions)
@@ -124,8 +149,16 @@ void CalculateDegs()
 
     //calculates servo degree steps so that each motor finishes movement at the same time
     for (int i = 0; i < 12; i++) {
-    servoDegSteps[i] = (desiredDegs[i] - currentDegs[i]) / 50;
+    servoDegSteps[i] = (desiredDegs[i] - currentDegs[i]) / STEP_SIZE;
   }
+}
+
+//Loops the supplied value between 0 and the supplied maximum
+int RangeLoop(int val, int max)
+{
+  if (val < max) return val;
+
+  return abs(val - max);
 }
 
 //maps the supplied degree value of the specified servo to a pwm value
@@ -164,6 +197,12 @@ Vector2D CoordToAngles(Vector2D *vec, int servoNumb) {
 }
 
 void loop() {
+
+//for general info data, a 0 is appended at the beginning of the message
+//Serial.print("0R: ");
+//Serial.print(sonicR.measureDistanceCm());
+//Serial.print(" / L: ");
+//Serial.println(sonicL.measureDistanceCm());
 
   int servosComplete = 0; //holds how many servos reached their final destination
 
